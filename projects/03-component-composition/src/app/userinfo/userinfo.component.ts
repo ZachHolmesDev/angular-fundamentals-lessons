5be5322ff8310c98c54f621737993708b268a538
zach <zholmes430@gmail.com>
2024-03-20 12:51:35 +1000

    Add UserinfoComponent to app.component.ts

---
+++ projects/03-component-composition/src/app/userinfo/userinfo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userinfo',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      userinfo works!
+    </p>
+  `,
+  styles: ``
+})
+export class UserinfoComponent {
+
+}
